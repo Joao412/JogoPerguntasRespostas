cfd8d4f983c05926af16db5ecf6091d4acbb3618
João Carlos <preto_412@hotmail.com>
2017-02-01 17:04:22 -0200

    Atualização do banco  de dados.

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -2,7 +2,8 @@ create database db_PerguntasERespostas
 
 use db_PerguntasERespostas
 
-create table tb_Jogador(
+create table tb_Jogador
+(
 	id int identity primary key,
 	nome varchar(100) not null
 )
@@ -11,3 +12,26 @@ select * from tb_Jogador
 
 insert into tb_Jogador(nome) values('Amanda')
 insert into tb_Jogador(nome) values('Fernanda')
+
+create table tb_Perguntas
+(
+	id int identity primary key,
+	pergunta varchar(max),
+	resposta_correta varchar(max),
+	data datetime default getdate()	
+)
+
+select * from tb_Perguntas
+
+insert into tb_Perguntas(pergunta, resposta_correta)
+values('Qual é a formula utilizada para se calcular a distorção causada pela gravidade infinita de um buraco negro?', 'Raio de Schwarschild')
+
+select * from tb_Jogador
+delete from tb_Jogador where id = 8
+insert into tb_Jogador(nome) values('Ramon')
+
+delete from tb_Jogador
+
+
+--Limpa os dados da tabela e reinicia o id.
+truncate table tb_Jogador
